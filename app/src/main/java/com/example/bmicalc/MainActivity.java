e751872a9f1936fe58958012a56585894e6124cc
kashish-1209 <kashish.maggu@mountblue.io>
2019-09-16 11:05:01 +0530

    functions completed

--- app/src/main/java/com/example/bmicalc/MainActivity.java
+++ app/src/main/java/com/example/bmicalc/MainActivity.java
@@ -2,13 +2,261 @@ package com.example.bmicalc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String KILOGRAM = "Kilograms";
+    private static final String POUND = "Pounds";
+    private static final String CENTIMETER = "Centimeter";
+    private static final String METER = "Meter";
+    private static final String FEET = "Feet";
+    private static final String INCH = "Inch";
+    private static final String HEIGHT_INPUT = "height_input";
+    private static final String WEIGHT_INPUT = "weight_input";
+    private static final String FINAL_RESULT = "final_result";
+    private static final String VISIBLITY_CALCULATOR = "visiblity_calculator";
+    private static final String HEIGHT_INPUT1 = "height_input";
+    private static final String WEIGHT_INPUT1 = "weight_input";
+    private static final String FINAL_RESULT1 = "final_result";
+    private static final String VISIBLITY_CALCULATOR1 = "visiblity_calculator";
+    String[] weightUnits = {KILOGRAM, POUND};
+    String[] heightUnits = {CENTIMETER, METER, FEET, INCH};
+    Spinner height_spinner;
+    Spinner weight_spinner;
+    TextView weight_unit;
+    TextView height_unit;
+    TextView output;
+    ArrayAdapter<String> adapterWeight;
+    ArrayAdapter<String> adapterHeight;
+    TextView sample_height;
+    double height_factor=1;
+    double weight_factor=1;
+    TextView sample_weight;
+    String weight_in = "";
+    String height_in = "";
+    Button zero, one, two, three, four, five, six, seven, eight, nine, dot, go, ac;
+    ImageButton del;
+    View calculator;
+    View result;
+    TextView category_weight;
+    float out;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        height_spinner = (Spinner) findViewById(R.id.spinner_height);
+        weight_spinner = (Spinner) findViewById(R.id.spinner_weight);
+        height_unit = (TextView) findViewById(R.id.heightUnit);
+        weight_unit = (TextView) findViewById(R.id.weightUnit);
+        adapterHeight = new ArrayAdapter<String>(getApplicationContext(), R.layout.itemfile, heightUnits);
+        height_spinner.setAdapter(adapterHeight);
+        adapterWeight = new ArrayAdapter<String>(getApplicationContext(), R.layout.itemfile, weightUnits);
+        weight_spinner.setAdapter(adapterWeight);
+        sample_height = (TextView) findViewById(R.id.sampleHeight);
+        sample_weight = (TextView) findViewById(R.id.sampleWeight);
+        zero = (Button) findViewById(R.id.button0);
+        one = (Button) findViewById(R.id.button1);
+        two = (Button) findViewById(R.id.button2);
+        three = (Button) findViewById(R.id.button3);
+        four = (Button) findViewById(R.id.button4);
+        five = (Button) findViewById(R.id.button5);
+        six = (Button) findViewById(R.id.button6);
+        seven = (Button) findViewById(R.id.button7);
+        eight = (Button) findViewById(R.id.button8);
+        nine = (Button) findViewById(R.id.button9);
+        dot = (Button) findViewById(R.id.buttondot);
+        go = (Button) findViewById(R.id.buttonGO);
+        ac = (Button) findViewById(R.id.buttonAC);
+        del = (ImageButton) findViewById(R.id.buttonDEL);
+        category_weight = (TextView) findViewById(R.id.category_weight);
+        calculator = findViewById(R.id.calculator_container);
+        result = findViewById(R.id.result_container);
+        output = findViewById(R.id.output);
+
+        sample_height.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                height_in="";
+                calculator.setVisibility(View.VISIBLE);
+                result.setVisibility(View.GONE);
+                sample_height.setTextColor(Color.parseColor("#ff9900"));
+                sample_weight.setTextColor(Color.parseColor("#919191"));
+            }
+        });
+        sample_weight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                weight_in="";
+                calculator.setVisibility(View.VISIBLE);
+                result.setVisibility(View.GONE);
+                sample_weight.setTextColor(Color.parseColor("#ff9900"));
+                sample_height.setTextColor(Color.parseColor("#919191"));
+            }
+        });
+
+        ArrayList<Button> list = new ArrayList<Button>();
+        list.add(zero);
+        list.add(one);
+        list.add(two);
+        list.add(three);
+        list.add(four);
+        list.add(five);
+        list.add(six);
+        list.add(seven);
+        list.add(eight);
+        list.add(nine);
+
+        for (int i = 0; i < list.size(); i++) {
+            list.get(i).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (Color.parseColor("#ff9900") == sample_height.getCurrentTextColor()) {
+                        if(height_in.equals("0"))
+                            height_in="";
+                        if (height_in.contains(".")) {
+                            if (height_in.length() < (height_in.indexOf('.') + 3)) {
+                                height_in += ((Button) view).getText();
+                                sample_height.setText(height_in);
+                            }
+                        } else {
+                            if (height_in.length() < 3) {
+                                height_in += ((Button) view).getText();
+                                sample_height.setText(height_in);
+                            }
+                        }
+                    } else if(Color.parseColor("#ff9900") == sample_weight.getCurrentTextColor()) {
+                        if(weight_in.equals("0"))
+                            weight_in="";
+                        if (weight_in.contains(".")) {
+                            if (weight_in.length() < (weight_in.indexOf('.') + 3)) {
+                                weight_in += ((Button) view).getText();
+                                sample_weight.setText(weight_in);
+                            }
+                        } else {
+                            if (weight_in.length() < 3) {
+                                weight_in += ((Button) view).getText();
+                                sample_weight.setText(weight_in);
+                            }
+                        }
+                    }
+                }
+            });
+        }
+        ac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Color.parseColor("#ff9900") == sample_height.getCurrentTextColor()) {
+                    height_in = "0";
+                    sample_height.setText(height_in);
+                } else if (Color.parseColor("#ff9900") == sample_weight.getCurrentTextColor()) {
+                    weight_in = "0";
+                    sample_weight.setText(weight_in);
+                }
+            }
+        });
+
+        dot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Color.parseColor("#ff9900") == sample_height.getCurrentTextColor()) {
+                    if(!height_in.contains(".")){
+                        height_in = sample_height.getText().toString();
+                        height_in+=".";
+                        sample_height.setText(height_in);
+                    }
+                } else {
+                    if(!weight_in.contains(".")){
+                        weight_in = sample_weight.getText().toString();
+                        weight_in+=".";
+                        sample_weight.setText(weight_in);
+                    }
+                }
+
+            }
+        });
+
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float a = Float.parseFloat(sample_height.getText().toString()) * (float) height_factor;
+                float b = Float.parseFloat(sample_weight.getText().toString()) * (float) weight_factor;
+                if (a == 0 || b == 0) {
+                    Toast.makeText(getApplicationContext(), "Enter correct information",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    out = b / (a * a/10000);
+                    output.setText(Float.toString(out));
+                    if (Float.parseFloat(output.getText().toString()) <= 18.5) {
+                        category_weight.setTextColor(getResources().getColor(R.color.Dark_orange));
+                        category_weight.setText("Underweight");
+                    } else if (Float.parseFloat(output.getText().toString()) <= 25.0) {
+                        category_weight.setText("Normal");
+                        category_weight.setTextColor(getResources().getColor(R.color.green));
+                    } else if (Float.parseFloat(output.getText().toString()) >= 50) {
+                        Toast.makeText(getApplicationContext(), "Invalid BMI! Please check your input",
+                                Toast.LENGTH_LONG).show();
+                        return;
+                    } else {
+                        category_weight.setText("Overweight");
+                        category_weight.setTextColor(getResources().getColor(R.color.darker_orange));
+                    }
+
+
+
+                //output.setTextColor("#");
+                calculator.setVisibility(View.GONE);
+                result.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+        del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (getResources().getColor(R.color.orange) == sample_height.getCurrentTextColor()) {
+                    height_in = sample_height.getText().toString();
+                    if (height_in.length() == 1) {
+                        height_in = "";
+                        height_in += "0";
+                        sample_height.setText(height_in);
+                    } else {
+                        StringBuilder builder = new StringBuilder(height_in);
+                        builder.deleteCharAt(height_in.length() - 1);
+                        height_in = "";
+                        height_in += builder;
+                        sample_height.setText(height_in);
+                    }
+                } else {
+                    weight_in = sample_weight.getText().toString();
+                    if (weight_in.length() == 1) {
+                        weight_in = "";
+                        weight_in += "0";
+                        sample_weight.setText(weight_in);
+                    } else {
+                        StringBuilder builder = new StringBuilder(weight_in);
+                        builder.deleteCharAt(weight_in.length() - 1);
+                        weight_in = "";
+                        weight_in += builder;
+                        sample_weight.setText(weight_in);
+                    }
+                }
+            }
+        });
+
+
     }
 }
